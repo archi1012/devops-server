cac6dc113bac2ad542b67c198ba596d1f0cc667d
archi1012 <142870151+archi1012@users.noreply.github.com>
2026-07-15 10:21:30 +0530

    fghjk

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "Exprebbbbbbbbbss server is running " });
+  res.json({ message: "Exprebbbbkikl;;bbbbbss server is running " });
 });
 
 app.listen(port, () => {
